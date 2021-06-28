29ea53ccce080d71117b7ec392e68c8953d663ba
huangqimin001 <huangqimin001@ke.com>
2021-06-29 03:08:17 +0800

    :art: Support param qrurl/qrdata

--- pywe_qrcode/__init__.py
+++ pywe_qrcode/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from pywe_qrcode.pyqrcode import QRCode, qrcode_create, qrcode_scene, qrcode_str_scene, qrcode_limit_scene, qrcode_limit_str_scene, qrcode_download, qrcode_show, qrcode_url
+from pywe_qrcode.pyqrcode import QRCode, qrcode_create, qrcode_scene, qrcode_str_scene, qrcode_limit_scene, qrcode_limit_str_scene, qrcode_data, qrcode_download, qrcode_show, qrcode_url
